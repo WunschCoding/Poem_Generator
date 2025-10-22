53831a3a13124d8ddf642cfc5796744cfa42a72a
Michaela <66178768+WunschCoding@users.noreply.github.com>
2025-10-22 13:55:10 +0200

    integrated AI api

---
+++ src/index.js
@@ -0,0 +1,61 @@
+function generatingPoem(event) {
+  event.preventDefault();
+  let inputPoemForm = document.querySelector("#input-text-form");
+  let poemHeadingElement = document.querySelector("#poem-heading");
+  poemHeadingElement.innerHTML = inputPoemForm.value;
+
+  let apiKey = "3412to3ec6a4dfdcbfe0195213b47c9a";
+  let context =
+    "Write a short original poem in English. Tone: Humorous. Imagery: pictorial. Length: 3 stanzas, 2 lines per stanza. Format the OUTPUT ONLY as HTML paragraphs with <p> for each stanza and <br> for each line break. Do NOT include a title, markdown, or extra text—HTML only.";
+
+  let prompt = inputPoemForm.value;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemByElement = document.querySelector("#poem-by");
+  poemByElement.innerHTML = "by ";
+  let poemAuthorElement = document.querySelector("#poem-author");
+  poemAuthorElement.innerHTML = "SheCodes AI";
+  let poemTextElement = document.querySelector("#poem-text");
+  poemTextElement.innerHTML = "Poem is being generated";
+
+  axios.get(apiUrl).then(displayPoem);
+  inputPoemForm.value = null;
+}
+
+function displayPoem(response) {
+  let poemText = response.data.answer;
+  let poemTextElement = document.querySelector("#poem-text");
+  poemTextElement.innerHTML = poemText;
+}
+
+function startPoem() {
+  let poemHeadingElement = document.querySelector("#poem-heading");
+  poemHeadingElement.innerHTML = "How Beautiful";
+  let poemByElement = document.querySelector("#poem-by");
+  poemByElement.innerHTML = "by ";
+  let poemAuthorElement = document.querySelector("#poem-author");
+  poemAuthorElement.innerHTML = "Mary Jo Bang";
+  let poemText = `<p>
+            A personal lens: glass bending rays <br />
+            That gave one that day’s news <br />
+            Saying each and every day,
+          </p>
+          <p>
+            Just remember you are standing <br />On a planet that’s evolving.
+            <br />How beautiful, she thought, what distance does
+          </p>
+          <p>
+            For water, the view from above or afar. <br />In last night’s dream,
+            they were back again <br />At the beginning. She was a child
+          </p>
+          <p>
+            And he was a child. <br />A plane lit down and left her there.
+            <br />Cold whitening the white sky whiter. <br />Then a scalpel cut
+            her open for all the world To be a sea.
+          </p>`;
+  let poemTextElement = document.querySelector("#poem-text");
+  poemTextElement.innerHTML = poemText;
+}
+
+startPoem();
+let submitPoemForm = document.querySelector("#poem-generator-form");
+submitPoemForm.addEventListener("submit", generatingPoem);
